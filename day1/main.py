72db170c74b0d95a007ffd4398a6b6186c448039
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2019-12-01 22:17:22 +0100

    Solutions for day1

---
+++ day1/main.py
@@ -0,0 +1,27 @@
+def calculate_fuel(mass,recursive=False):
+    if (recursive):
+        result = int(mass/3)-2
+        fuel = result
+        while((int(fuel/3)-2)>0):
+            fuel= int(fuel/3)-2
+            result+= fuel
+        return result
+
+    else:
+        result = int(mass/3)-2
+        return result
+
+if __name__ == "__main__":
+    with open("input.txt", mode='r') as f:
+        content = f.readlines()
+        content = [int(x) for x in content] 
+    
+    content_p1 = [calculate_fuel(x) for x in content]
+    print("Part 1 result: {}".format(sum(content_p1))) #Result for p1
+    content_p2 = [calculate_fuel(x,recursive=True) for x in content]
+    print("Part 2 result: {}".format(sum(content_p2))) #Result for p2
+
+
+
+
+
