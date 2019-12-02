c7f4995288c3606c5e45765766b840a58c746e7b
Jaime Ferrando Huertas <jaime.ferrando_huertas@sandvik.com>
2019-12-02 10:41:21 +0000

    day2 solution and new readme

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,5 @@
+#1 line solution for p1: sum(int(x)//3-2 for x in open('input.txt'))
+
 def calculate_fuel(mass,recursive=False):
     result = int(mass/3)-2
     if (recursive):
@@ -7,11 +9,11 @@ def calculate_fuel(mass,recursive=False):
             result+= fuel
     return result
 
+def read_input(path):
+    return [int(x) for x in open('input.txt')]
+
 if __name__ == "__main__":
-    with open("input.txt", mode='r') as f:
-        content = f.readlines()
-        content = [int(x) for x in content] 
-    
+    content = read_input("input.txt")
     content_p1 = [calculate_fuel(x) for x in content]
     print("Part 1 result: {}".format(sum(content_p1))) #Result for p1
     content_p2 = [calculate_fuel(x,recursive=True) for x in content]
---
+++ day2/main.py
@@ -0,0 +1,44 @@
+ops = {
+    1: lambda x, y: x+y,
+    2: lambda x, y: x*y
+}
+
+def int_code(code):
+    i=0
+    while(i+4<len(code)):
+        if(code[i] in ops.keys()):
+            a = code[code[i+1]]
+            b = code[code[i+2]]
+            target = code[i+3]
+            #op
+            code[target] = ops[code[i]](a,b)
+            i+=4
+        else:
+            i+=1
+    return code
+
+def read_input(path):
+    return [int(x) for x in  open('input.txt').read().split(',')]
+
+if __name__ == "__main__":
+    #Read the input
+    content = read_input("input.txt")
+    #Follow problem instructions to replace values
+    content_p1 = content.copy()
+    content_p1[1]=12
+    content_p1[2]=2
+    #Solve the intcode
+    result = int_code(content_p1)
+    print("Result for p1: {}".format(result[0]))
+    #Solve p2 brute force
+    for noun in range(99):
+        for verb in range(99):
+            content_p2 = content.copy()
+            content_p2[1]=noun
+            content_p2[2]=verb
+            r = int_code(content_p2)
+            if(r[0]==19690720):
+                print("Noun:{}  Verb:{}  Result for p2: {}".format(noun,verb,100*noun+verb))
+
+
+
